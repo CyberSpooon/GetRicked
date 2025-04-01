a7669a0997c59ddb644ce6887cf0cfc233138819
CyberSpooon <137320651+CyberSpooon@users.noreply.github.com>
2025-04-01 14:42:04 -0400

    Update GetRicked.ps1

--- GetRicked.ps1
+++ GetRicked.ps1
@@ -3,4 +3,4 @@ $audio = "https://github.com/CyberSpooon/GetRicked/raw/refs/heads/main/Get_Ricke
 $path = "$env:TEMP\Get_Ricked.mp3"
 Start-Process curl.exe -WindowStyle Hidden -ArgumentList "-L $audio -o $path" ## Download .mp3
 Start-Sleep -Seconds 3 ## wait for .mp3 to download
-Start-Process "wmplayer.exe" -ArgumentList "$path" -WindowStyle Hidden # open .mp3 in hidden media player window
+Start-Process "wmplayer.exe" -ArgumentList "$path" -WindowStyle Hidden ## open .mp3 in hidden media player window
