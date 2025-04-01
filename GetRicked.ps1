4e0ba79c842edc8aabddd7aece4eebe6bf6858c6
CyberSpooon <137320651+CyberSpooon@users.noreply.github.com>
2025-04-01 14:39:10 -0400

    Update GetRicked.ps1
    formatting changes and comments

--- GetRicked.ps1
+++ GetRicked.ps1
@@ -1,7 +1,6 @@
 Set-ExecutionPolicy -Scope CurrentUser Bypass
 $audio = "https://github.com/CyberSpooon/GetRicked/raw/refs/heads/main/Get_Ricked.mp3"
 $path = "$env:TEMP\Get_Ricked.mp3"
-Start-Process curl.exe -WindowStyle Hidden -ArgumentList "-L $audio -o $path" 
-Start-Sleep -Seconds 3
-Start-Process "wmplayer.exe" -ArgumentList "$path" -WindowStyle Hidden
-Start-Process cmd.exe -ArgumentList "/c curl.exe ascii.live/rick"
+Start-Process curl.exe -WindowStyle Hidden -ArgumentList "-L $audio -o $path" ## Download .mp3
+Start-Sleep -Seconds 3 ## wait for .mp3 to download
+Start-Process "wmplayer.exe" -ArgumentList "$path" -WindowStyle Hidden # open .mp3 in hidden media player window
